1f9f5186076cff8571cbd1738de25333f8df41ec
= <=>
2024-03-12 17:12:04 -0300

    Adição do Laravel

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\http\Controllers\UserController;
+use App\http\Controllers\PagesController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+Route::get("/user",[UserController::class,'index'])->name('user.index');
+Route::get("/user/create", [UserController::class,'create'])->name('User.create');
+Route::get("/board", [PagesController::class,'index'])->name('board');
